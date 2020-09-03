a0f01e30fe206052480eb18947bdafb4f22ef719
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-09-03 09:07:56 -0700

    added boiler plate to server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+require('dotenv').config()
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://" + process.env.MONGOUSER + ":" + process.env.MONGOPASS + "@cluster0.ddnbo.azure.mongodb.net/contact?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
